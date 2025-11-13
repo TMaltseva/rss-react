eecf5e7439326fd6d3c72918ad639a14fd574ced
TMaltseva <bacardeonie@gmail.com>
2025-11-13 02:06:14 -0800

    chore: add next js config

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = withNextIntl(nextConfig);
